5f610c1bfffe6dd61a1b5cb5045064dee03b9d5a
Rémy Guillaumot <Rmy Guillaumot>
2017-03-30 10:12:52 -0400

    aspectGraphique

--- MMI/aspectGraphique/Tp3TechWeb/functions.php
+++ MMI/aspectGraphique/Tp3TechWeb/functions.php
@@ -11,6 +11,6 @@
 function erreur($err=''){
 	$message=($err!='')? $err:'Une erreur inconnue s\'est produite';
 	exit('<p>'.$mess.'<\p>
-	<p>Cliquez <a href="./index.php">ici</a> pour revenir à la page d\'accueil</p></div></body></html>');
+	<p>Cliquez <a href="./indexR.php">ici</a> pour revenir à la page d\'accueil</p></div></body></html>');
 }
 ?>
